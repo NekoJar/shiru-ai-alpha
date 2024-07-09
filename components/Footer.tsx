a537b8a363134c9ea94774cc47880d2b5801f1df
NekoJar <mzharfanhakim7@gmail.com>
2024-07-09 11:07:07 +0700

    feat:adding visitor chart

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <p>Built by shadcn. The source code is available on GitHub.</p>
+    </div>
+  );
+};
