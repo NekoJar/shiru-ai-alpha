fdb1d01023cd076ff183d2d6c2f2b4c79281417a
NekoJar <mzharfanhakim7@gmail.com>
2024-08-08 11:58:29 +0700

    feat:updating deployment for supabase only option

--- app/api/peoples/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const people = await prisma.peoples.findMany({ orderBy: { id: "asc" } });
-
-  return NextResponse.json(people);
-}
